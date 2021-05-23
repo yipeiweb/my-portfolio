8ab4dcb630e786ab0108af039c3858e3b254d3ba
yipeiweb <peiyiliu88@gmail.com>
2021-05-23 11:58:04 +0200

    Version 1.1

--- src/controllers/ContainerController.js
+++ src/controllers/ContainerController.js
@@ -5,6 +5,40 @@ export default class {
     constructor(languageManager)
     {
         this.languageManager = languageManager;
+        this.languageTag = languageManager.getLanguage()
+    }
+
+    printSeeProjectLink(project)
+    {
+        const self = this;
+        if (project.hasOwnProperty('projectLink')) {
+            return <a href={project.projectLink} className="btn btn-outline-success m-2" target="_blank">
+                        {container.projectLink[self.languageTag]}
+                    </a>
+        }
+
+        return ''
+    }
+
+    printSeeProjectRepository(project)
+    {
+        const self = this;
+        if (project.hasOwnProperty('projectRepository')) {
+            return <a href={project.projectRepository} className="btn btn-outline-success m-2" target="_blank">
+                    {container.projectRepository[self.languageTag]}
+                   </a>
+        }
+
+        return ''
+    }
+
+    printImage(project)
+    {
+        if (project.hasOwnProperty('image')) {
+            return <img loading="lazy" src={project.image} width="100%" />
+        }
+
+        return '';
     }
 
     printUseTecnologies(useTecnologies)
@@ -18,23 +52,18 @@ export default class {
 
     printProjects()
     {
-        const languageTag = this.languageManager.getLanguage();
         const self = this;
 
         return Object.keys(projects).map(index =>
-            <div className="col-md-6">
+            <div className="col-md-6 mb-4">
                 <div className="card" key={index}>
-                    <img loading="lazy" src={projects[index].image} width="100%" />
+                    {self.printImage(projects[index])}
                     <div className="card-body">
-                        <h5 className="card-title">{projects[index].title[languageTag]}</h5>
+                        <h5 className="card-title">{projects[index].title[self.languageTag]}</h5>
                         <span>{self.printUseTecnologies(projects[index].useTecnologies)}</span>
-                        <p className="card-text">{projects[index].description[languageTag]}</p>
-                        <a href={projects[index].projectLink} className="btn btn-outline-success m-2" target="_blank">
-                            {container.projectLink[languageTag]}
-                        </a>
-                        <a href={projects[index].projectRepository} className="btn btn-outline-success m-2" target="_blank">
-                            {container.projectRepository[languageTag]}
-                        </a>
+                        <p className="card-text">{projects[index].description[self.languageTag]}</p>
+                        {self.printSeeProjectLink(projects[index])}
+                        {self.printSeeProjectRepository(projects[index])}
                     </div>
                 </div>
             </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -12,5 +12,33 @@ export default [
         projectLink: 'https://yipeiweb.github.io/my-card/',
         projectRepository: 'https://github.com/yipeiweb/my-card',
         image: 'https://drive.google.com/uc?id=1vZXRjxDj2XQsqhkKXSNOhjB3QXlNcYtb'
-    }
+    },
+    {
+        title: {
+            en: 'My portfolio',
+            es: 'Mi portafolio'
+        },
+        description: {
+            en: "My online portfolio where I list all my developed projects, available in multi-language",
+            es: 'Mi portafolio online dónde listo todos mis proyectos desarrollados, disponible en multi idioma.'
+        },
+        useTecnologies: ['Bootstrap', 'React'],
+        projectLink: 'https://yipeiweb.github.io/my-portfolio/',
+        projectRepository: 'https://github.com/yipeiweb/my-portfolio',
+        image: 'https://drive.google.com/uc?id=1BVace0zof27qC2xyiWaHO5xwYnrxpEtK'
+    },
+    {
+        title: {
+            en: 'My blog',
+            es: 'Mi blog'
+        },
+        description: {
+            en: "【Project under development】Blog developed from 0 where I publish content about the world of computing",
+            es: '【Proyecto en desarrollo】Blog desarrollado desde 0 dónde publico contenidos sobre mundo de informàtica'
+        },
+        useTecnologies: ['Bootstrap', 'Symfony', 'Angular'],
+        //projectLink: 'https://yipeiweb.github.io/my-portfolio/',
+        // projectRepository: 'https://github.com/yipeiweb/my-portfolio',
+        //image: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/SITIO-EN-CONSTRUCCION.jpg'
+    },
 ]
