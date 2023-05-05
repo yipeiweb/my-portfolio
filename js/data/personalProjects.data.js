05bfeff05204a3498b2faecef4fd2a0f8d8b0a38
YiPeiWeb <peiyiliu88@gmail.com>
2023-05-06 00:03:43 +0200

    [06/05/2023]Add new project and new about me website

--- js/data/personalProjects.data.js
+++ js/data/personalProjects.data.js
@@ -1,5 +1,13 @@
 export default [
     {
+        title: 'Gestor de tareas pendiente',
+        imageUrl: 'https://drive.google.com/uc?id=1mn8mzXVYeZbAsU80FM_dZBtpS5ErNsFe',
+        webUrl: 'https://64557695e6b85218ac55a0a8--spontaneous-conkies-5c9d70.netlify.app/',
+        repositoryUrl: 'https://github.com/yipeiweb/react-redux_toolkit-taskmanager',
+        useTechnologies: 'Tailwindcss, React(Redux toolkit, React Router Dom), Localstorage',
+        description: 'Un mini gestor de tareas pendiente a realizar.'
+    },
+    {
         title: 'Quiosco de café',
         imageUrl: 'https://drive.google.com/uc?id=1dbb2r6tNQ8-DnP6i4OI9-_PmlgEkmEvp',
         webUrl: 'https://6448e610fd87a43a07896926--cool-duckanoo-4ceb41.netlify.app/',
@@ -76,6 +84,6 @@ export default [
         webUrl: 'https://yipeiweb.github.io/my-portfolio',
         repositoryUrl: 'https://github.com/yipeiweb/my-portfolio',
         useTechnologies: 'Bootstrap, Javascript',
-        description: 'Portafolio de proyectos que he desarrollado por mi cuenta y en la empresa.'
+        description: 'Portafolio de proyectos que he desarrollado, con un sistema de enrutamiento desarrollado desde 0.'
     }
 ]
